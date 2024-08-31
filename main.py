371a31c219a5b8d24a29edc4c7b16b48bea137d7
Omar Haddad <omarhaddad@MacBook-Pro-de-Omar-Haddad.local>
2024-08-31 13:16:15 +0200

    fix analyse data function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.data_preprocessing import preprocess_data
 from src.data_analysis import analyze_data
-from src.data_visualisation import visualize_data
+
 
 def main():
     # Prétraitement des données
@@ -9,8 +9,6 @@ def main():
     # Analyse des données
     analyze_data('data/processed/processed_data.csv')
 
-    # Visualisation des données
-    visualize_data('data/processed/processed_data.csv')
 
 if __name__ == "__main__":
     main()
